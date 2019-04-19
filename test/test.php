4a920bf62591df0c56cb5fd9c9de254389908aa4
Snail <644516361@qq.com>
2019-04-19 19:20:49 +0800

    update

--- test/test.php
+++ test/test.php
@@ -1,7 +1,3 @@
-
-<input type="text" placeholder="验证码" name="verifycode" class="captcha"><br><br>
-<img id="captcha_img" src="index.php?r=<?php echo rand();?>" alt="验证码">
-<label><a href="javascript:void(0)" onclick="document.getElementById('captcha_img').src='index.php?r='+Math.random()">换一个</a> </label>
 <?php 
-phpinfo();
+var_dump(is_file('a_file.txt'));
  ?>
