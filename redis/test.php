1918ea007a74337bf05518c2d3f09a60d0726849
Snail <644516361@qq.com>
2019-09-11 22:06:12 +0800

    'update'

--- redis/test.php
+++ redis/test.php
@@ -1,4 +1,5 @@
-<?php  
+<?php
+	//数据库连接
 	$mysql_conf = array(
     'host'    => '127.0.0.1:3306', 
     'db'      => 'pm_dev', 
@@ -17,27 +18,26 @@
 	    die("could not connect to the db:\n" .  $mysqli->error);
 	}
 
-	$sql = "select * from admin_user where id = 1;";
-	$res = $mysqli->query($sql);
-	if (!$res) {
-	    die("sql error:\n" . $mysqli->error);
-	}
-	while ($row = $res->fetch_assoc()) {
-	    var_dump($row);
-	}
-
-	
-
 	//连接本地的 Redis 服务
 	$redis = new Redis();
 	$redis->connect('127.0.0.1', 6379);
-	echo "Connection to server successfully";
-	//设置 redis 字符串数据
-	$redis->set("tutorial-name", "Redis tutorial");
-	// 获取存储的数据并输出
-	echo "Stored string in redis:: " . $redis->get("tutorial-name");
 
+	//获取get参数
+	$id = isset($_GET['id']) ? $_GET['id'] : 1;
 
-
-	$res->free();
+	if ($result = $redis->get("id_{$id}")) {
+		var_dump(unserialize($result));
+	} else {
+		//数据库操作
+		$sql = "select * from admin_user where id = {$id};";
+		$res = $mysqli->query($sql);
+		if (!$res) {
+		    die("sql error:\n" . $mysqli->error);
+		}
+		$row = $res->fetch_assoc();
+		$redis->set("id_{$id}",serialize($row),60);
+		var_dump($row);
+		$res->free();
+	}
+	
 	$mysqli->close();
--- test/test.php
+++ test/test.php
@@ -1,3 +1,3 @@
 <?php 
-var_dump(is_file('a_file.txt'));
+var_dump($_POST);
  ?>
