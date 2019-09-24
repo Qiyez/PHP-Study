1bce2c004e308ed22067ee67df81d82b869a3813
Snail <644516361@qq.com>
2019-09-24 20:10:29 +0800

    update

--- test/index.php
+++ test/index.php
@@ -1,43 +1,7 @@
 <?php  
-	$mysql_conf = array(
-    'host'    => '127.0.0.1:3306', 
-    'db'      => 'pm_dev', 
-    'db_user' => 'root', 
-    'db_pwd'  => '123456', 
-    );
 
-	$mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
-	if ($mysqli->connect_errno) {
-	    die("could not connect to the database:\n" . $mysqli->connect_error);//诊断连接错误
-	}
-	$mysqli->query("set names 'utf8';");//编码转化
-	$select_db = $mysqli->select_db($mysql_conf['db']);
+$arr1 = [1,3,5,7,9];
+$arr2 = [1,2,3,4,5,6];
 
-	if (!$select_db) {
-	    die("could not connect to the db:\n" .  $mysqli->error);
-	}
-
-	$sql = "select * from admin_user where id = 1;";
-	$res = $mysqli->query($sql);
-	if (!$res) {
-	    die("sql error:\n" . $mysqli->error);
-	}
-	while ($row = $res->fetch_assoc()) {
-	    var_dump($row);
-	}
-
-	
-
-	//连接本地的 Redis 服务
-	$redis = new Redis();
-	$redis->connect('127.0.0.1', 6379);
-	echo "Connection to server successfully";
-	//设置 redis 字符串数据
-	$redis->set("tutorial-name", "Redis tutorial");
-	// 获取存储的数据并输出
-	echo "Stored string in redis:: " . $redis->get("tutorial-name");
-
-
-
-	$res->free();
-	$mysqli->close();
+$result = array_diff($arr2, $arr1);
+var_dump($result);
